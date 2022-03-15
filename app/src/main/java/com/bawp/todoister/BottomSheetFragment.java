bed556f47f9bc0ce89516cc611f9b5c4138ba6d0
YeshayahuBennun <ybennun@yahoo.com>
2022-03-15 19:00:20 +0200

    commit#13 - Todoister App -  Setup Chip Widgets and Getting Dates

--- app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
+++ app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
@@ -27,7 +27,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import java.util.Calendar;
 import java.util.Date;
 
-public class BottomSheetFragment extends BottomSheetDialogFragment {
+public class BottomSheetFragment extends BottomSheetDialogFragment implements View.OnClickListener {
     private EditText enterTodo;
     private ImageButton calendarButton;
     private ImageButton priorityButton;
@@ -60,8 +60,11 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
 
 
         Chip todayChip = view.findViewById(R.id.today_chip);
+        todayChip.setOnClickListener(this);
         Chip tomorrowChip = view.findViewById(R.id.tomorrow_chip);
+        tomorrowChip.setOnClickListener(this);
         Chip nextWeekChip = view.findViewById(R.id.next_week_chip);
+        nextWeekChip.setOnClickListener(this);
 
         return view;
     }
@@ -94,6 +97,28 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
                 TaskViewModel.insert(myTask);
             }
         });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.today_chip) {
+            //set the date for today
+            calendar.add(Calendar.DAY_OF_YEAR, 0);//the zero represents today
+            dueDate = calendar.getTime();
+            Log.d("TIME", "onClick: "+dueDate.toString());
+        } else if (id == R.id.tomorrow_chip) {
+            //set the date for tomorrow
+            calendar.add(Calendar.DAY_OF_YEAR, 1);//1 represents tomorrow
+            dueDate = calendar.getTime();
+            Log.d("TIME", "onClick: "+dueDate.toString());
+        } else if (id == R.id.next_week_chip) {
+            //set the date for next week
+            calendar.add(Calendar.DAY_OF_YEAR, 7);//7 represents next week
+            dueDate = calendar.getTime();
+            Log.d("TIME", "onClick: "+dueDate.toString());
+        }
 
     }
 }
